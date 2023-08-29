38453609971ed76c6ddde78a79c9bb6d5511428f
Bruno Ilha Carneiro <126731354+Brunoilha@users.noreply.github.com>
2023-08-29 19:21:22 -0300

    Update exercicio01.py

--- aula03/exercicio01.py
+++ aula03/exercicio01.py
@@ -1,11 +1,14 @@
 1. Faç#a um programa para gerar automaticamente números entre 0 e 99 de uma cartela de bingo. Sabendo que cada cartela devera conter 5 linhas de 5 números, gere estes dados de modo a não ter números repetidos dentro das cartelas. O programa deve exibir na tela a cartela gerada.
 
+
 import random
 matriz = [[],[],[],[],[]]
-
+volue = []
 
 for i in range(5):
     for j in range(5):
-        matriz[i].append(random.randint(0,99))
+        if i not in matriz:
+            volue = random.randint(0,99)
+            matriz[i].append(random.randint(0,99))
 
 print(matriz)
