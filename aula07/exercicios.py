bccb9d2cab32e0f1990a9af2caa2e676dd1719d7
Bruno Ilha Carneiro <126731354+Brunoilha@users.noreply.github.com>
2023-10-10 21:46:27 -0300

    Update exercicios.py

--- aula07/exercicios.py
+++ aula07/exercicios.py
@@ -1,4 +1,4 @@
-
+#Crie um programa que receba através de input dois números e retorne sua divisão.
 def division(num1,num2):
      try:
         return num1 / num2
@@ -30,3 +30,33 @@ def main():
         print(f'[error] ocorreu um erro: {erro} ')
             
 main()
+
+#Crie um programa que receba através de um input o valor numérico de um mês e retorne seu valor escrito. Lembre de tratar as exceções do seu programa. Exemplo: 1 -> Janeiro, 12 -> Dezembro
+class InvalidMonthError(BaseException):
+    pass 
+def get_month(month):
+    try:
+        months = ("janeiro","fevereiro","março","abril","maio","junho","julho","agosto","setembro","outubro","novembro","dezembro")
+        return month[month -1]
+    except IndexError:
+        print('[error] tente novamente com numeros inteiros')
+    except BaseException as error:
+        print(f'[erro] ocorreu um erro: {error} ')
+
+
+def main():
+    month = int(input("digite um numero para saber o mes conrrespondete: "))
+    if month > 12 or month < 1:
+        raise InvalidMonthError
+    month_name = get_month(month)
+    print('o mes é',month_name )
+    except ValueError:
+        print('[erro] tente novamente com numeros inteiros')
+    except InvalidMonthError:
+        print('[error] o mes informado nao existe')
+    except BaseException as error:
+        print(f'[erro] ocorreu um erro: {error} ')
+        
+main()
+    
+    
